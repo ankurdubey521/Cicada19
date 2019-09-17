daa955f4919b8e8df2244fff879ac36c42c2d305
Himanshu Gangwani <44908400+Himanshukr000@users.noreply.github.com>
2019-09-17 23:32:36 +0530

    Add files via upload

---
+++ HimanshuKr000/UserAgent/files/index.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+<h1 style="color: blue;">You ARE not the Right AGENT.</h1>
+<p>Bond IS Known AS "MI6"</p>
+
+</body>
+</html>
+<?php
+if ($_SERVER['HTTP_USER_AGENT']==='MI6') {
+    echo '3301{Y0u_Ar3_4lw4y5_Righ7_Ag3n7007}';
+}
+?>
